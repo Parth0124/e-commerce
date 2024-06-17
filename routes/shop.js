7fd50e317e1fb62e3fdcd8d7a8059aafd5355fc7
Abhiraj412 <abhangparth@gmail.com>
2024-06-17 21:54:29 +0530

    changed the styling and shared data across requests and users to the console

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,11 @@ const path = require('path')
 
 const rootDir = require('../util/path')
 
-const router = express.Router()
+const router = express.Router();
+const adminData = require('./admin')
 
 router.get('/', (req,res,next) => {
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
