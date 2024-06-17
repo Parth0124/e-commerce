8d75d27164f21b7b448fccb9d1a620599914978c
Abhiraj412 <abhangparth@gmail.com>
2024-06-17 22:06:51 +0530

    used html handling template pug

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({extended: false}));
