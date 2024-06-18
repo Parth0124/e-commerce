7929d263aa8a9ec5a3ad192958a20b9e569c7cb0
Abhiraj412 <abhangparth@gmail.com>
2024-06-18 13:43:00 +0530

    added pug as html template and used it to make the shop.html page

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,8 @@ const router = express.Router();
 const adminData = require('./admin')
 
 router.get('/', (req,res,next) => {
-    res.render('shop');
+    const products = adminData.products
+    res.render('shop', {prods: products, docTitle: 'Shop'});
 })
 
 module.exports = router
