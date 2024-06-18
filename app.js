267e2da17b6025c587e8b7bf00ebe82ce6814ce0
Abhiraj412 <abhangparth@gmail.com>
2024-06-19 03:11:19 +0530

    code before adding controlers

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoutes = require('./routes/shop')
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')))
