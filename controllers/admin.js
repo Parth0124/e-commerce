0870d3277716272f54e746d9cc112d7b69ce7368
Abhiraj412 <abhangparth@gmail.com>
2024-06-21 12:13:56 +0530

    edit product page formed

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -19,6 +19,19 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req,res,next) => {
+  const editMode = req.query.edit;
+  if(!editMode)
+  {
+    return res.redirect('/');
+  }
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    editing: editMode
+  });
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
