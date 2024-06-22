e907408f4a6bd8fa20bd5009449fa68b126da244
Abhiraj412 <abhangparth@gmail.com>
2024-06-22 22:44:18 +0530

    set up the connection for sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'e_commerce',
-    password: 'Parth0124'
-})
+const sequelize = new Sequelize("e_commerce", "root", "Parth0124", {
+  dialect: "mysql",
+  host: localhost,
+});
 
-module.exports = pool.promise();
+module.exports = sequelize
