183d75d076c31550ca80771e7532d2cb1ebac776
Abhiraj412 <abhangparth@gmail.com>
2024-06-24 18:48:19 +0530

    fetched the related products of the user

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const { constants } = require('buffer');
 
 const app = express();
 
@@ -15,13 +18,38 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    // console.log(result)
+Product.belongsTo(User, {constants:true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize.sync()
+.then(result => {
+    return User.findByPk(1)
+})
+.then(user => {
+    if(!user)
+    {
+        return User.create({name: "Max", email: 'test@xyz.com'});
+    }
+    return user;
+})
+.then(user => {
+    console.log(user);
 })
 .catch(err => {
     console.log(err)
