fb559b4505d9e2fd7fbf7d57cdb750a984144ad0
Abhiraj412 <abhangparth@gmail.com>
2024-06-25 14:04:25 +0530

    added the orders table

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 const { constants } = require('buffer');
 
 const app = express();
@@ -44,6 +46,10 @@ Cart.belongsTo(User);
 
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem})
 
 sequelize.sync()
 .then(result => {
--- models/order-item.js
+++ models/order-item.js
@@ -2,13 +2,16 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const Order = sequelize.define('order', {
+const OrderItem = sequelize.define('orderItem', {
   id: {
     type:Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true
+  },
+  quantity: {
+    type: Sequelize.INTEGER
   }
 })
 
-module.exports = Order;
+module.exports = OrderItem;
