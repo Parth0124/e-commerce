f7ab57faec470d083f5b3b09d5d302c50e68879c
Abhiraj412 <abhangparth@gmail.com>
2024-07-03 19:10:48 +0530

    completed working with database.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("66843086509f37ee46f0b8ef")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("66852d53362fe6925c3cc038")
+    .then(user => {
+      req.user = user
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +38,20 @@ mongoose
     "mongodb+srv://abhangparth:ParthECommerce@cluster0.c2oyrqb.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then((result) => {
+    User.findOne()
+    .then(user => {
+      if(!user)
+        {
+        const user = new User({
+          name: "Parth",
+          email: 'abhangparth@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
     app.listen(3000);
     console.log("Connected to database and server is running live")
   })
