cf72b4ca290f5d91b96844dac2821e859387d82d
Abhiraj412 <abhangparth@gmail.com>
2024-07-08 12:11:47 +0530

    cookie added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
-        res.render("auth/login", {
-          path: "/login",
-          pageTitle: "Login",
-        });
-  };
+    const isLoggedIn = req.get("Cookie").split(";")[4].trim().split("=")[1] === 'true';
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  res.redirect("/");
+};
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,6 @@ const authController = require('../controllers/auth')
 
 router.get('/login', authController.getLogin);
 
+router.post('/login', authController.postLogin);
+
 module.exports = router;
