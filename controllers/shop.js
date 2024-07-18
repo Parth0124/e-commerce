bf6a1d0c3abd0edc2b267db4578166d77cd12dd4
Abhiraj412 <abhangparth@gmail.com>
2024-07-18 15:18:58 +0530

    commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -116,7 +116,6 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then(result => {
-      console.log(result);
       res.redirect('/cart');
     })
     .catch(err => {
